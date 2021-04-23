f1d7f18f5cde7e35dee1118bf495451d15112851
rajkumbhar <rajkumb.rk@gmail.com>
2021-04-23 15:12:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+
+var tip = 0;
+function setFeedback(feedback) {
+
+    document.getElementById('drop-button').innerHTML = feedback;
+    var btn = document.getElementById('drop-button');
+    
+    if(feedback == "Outstanding 🤩") {
+         tip = 30;
+    }
+    else if(feedback == "Good 😃") {
+         tip = 20;
+    }
+    else if(feedback == "It was Ok 😀") {
+         tip = 15;
+    }
+    else if(feedback == "Bad ☹️") {
+         tip = 10;
+    }
+    else if(feedback == "Terrible 😤") {
+         tip = 5;
+    }
+
+    
+} 
+
+function calculate() {
+    var amount = document.getElementById('amount').value;
+    var people = document.getElementById('people').value;
+
+    var tipAmount = (amount * tip) / 100;
+    var totalAmount =  eval(tipAmount) + eval(amount);
+    var perHeadAmt = totalAmount / people;
+
+    document.getElementById('totalAmount').innerHTML = "Total Amount : Rs. "+ totalAmount+"/-";
+    document.getElementById('PerHead').innerHTML = "Per person Share : Rs. "+ perHeadAmt+"/-"; 
+}
