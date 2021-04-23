da9bc16059c170e06cc987c79893c965ee026cf1
rajkumbhar <rajkumb.rk@gmail.com>
2021-04-23 19:59:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,9 @@
 var tip = 0;
 function setFeedback(feedback) {
 
+
+
+
     document.getElementById('drop-button').innerHTML = feedback;
     var btn = document.getElementById('drop-button');
     
@@ -14,7 +17,7 @@ function setFeedback(feedback) {
     else if(feedback == "It was Ok 😀") {
          tip = 15;
     }
-    else if(feedback == "Bad ☹️") {
+    else if(feedback == "Bad 😖") {
          tip = 10;
     }
     else if(feedback == "Terrible 😤") {
@@ -24,18 +27,23 @@ function setFeedback(feedback) {
     
 } 
 
+function check(event) {
+     event.preventDefault();
+}
+
 function calculate() {
+
 
     var amount = document.getElementById('amount').value;
     var people = document.getElementById('people').value;
 
     if (amount > 0 && people > 0) {
-        var tipAmount = (amount * tip) / 100;
+   var tipAmount = (amount * tip) / 100;
     var totalAmount =  eval(tipAmount) + eval(amount);
     var perHeadAmt = totalAmount / people;
 
-    document.getElementById('totalAmount').innerHTML = "Total Amount : Rs. "+ totalAmount+"/-";
-    document.getElementById('PerHead').innerHTML = "Per person Share : Rs. "+ perHeadAmt+"/-"; 
+    document.getElementById('totalAmount').innerHTML = "Total Amount : Rs. "+ Math.round(totalAmount)+"/-";
+    document.getElementById('PerHead').innerHTML = "Per person Share : Rs. "+ Math.round(perHeadAmt)+"/-"; 
     }
     else{
         alert("Please check Bill Amount or Number of people");
